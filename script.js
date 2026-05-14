4a3e18741a6992fcfed43f864bba6d3a8b72eb8b
Olivia <olivia@Olivias-MacBook-Pro.local>
2026-05-14 21:26:24 +0200

    Initial dog food store

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.site-nav');
+
+menuToggle.addEventListener('click', () => {
+    const isOpen = nav.classList.toggle('open');
+    menuToggle.setAttribute('aria-expanded', String(isOpen));
+});
